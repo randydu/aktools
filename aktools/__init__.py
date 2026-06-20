6b1469eea24640a7b4c29f2a778714dcc4eeaa02
Gogs <gogs@fake.local>
2026-06-19 22:46:27 -0700

    chore: release v0.1.2

--- aktools/__init__.py
+++ aktools/__init__.py
@@ -102,8 +102,9 @@ Changelog
 0.0.97: add: HK, LOF, open-fund, futures, index, bond APIs + per-key cache control
 0.1.0: first stable release of universal v1 API layer
 0.1.1: fix logger NameError at startup + cache key migration
+0.1.2: fix Dockerfile local source build + .dockerignore
 """
 
 __title__ = "AKTools"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __author__ = "AKFamily"
